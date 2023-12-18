883d7c726150d4878643c6c4460b3b765a3e98d3
Manuel Salazar <manalgom@gmail.com>
2023-12-18 11:21:49 -0500

    Using serializers for the api

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -7,40 +7,25 @@ from rest_framework.response import Response
 # Models
 from cride.circles.models import Circle
 
+# Serializers
+from cride.circles.serializers import (
+    CircleSerializer,
+    CreateCircleSerializer
+)
+
 @api_view(['GET'])
 def list_circles(request):
     """List circles"""
 
     circles = Circle.objects.filter(is_public=True)
-    # import ipdb; ipdb.set_trace()
-
-    data = []
-
-    for circle in circles:
-        data.append({
-            'name':circle.name,
-            'slug_name':circle.slug_name,
-            'rides_taken':circle.rides_taken,
-            'rides_offered':circle.rides_offered,
-            'members_limit':circle.members_limit
-
-        })
-    return Response(data)
+    serializer = CircleSerializer(circles, many=True)
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def create_circle(request):
     """Create Circle"""
 
-    name = request.data['name']
-    slug_name = request.data['slug_name']
-    about = request.data.get('about','') # Remember it is a dict
-    circle = Circle.objects.create(name=name, slug_name=slug_name,about=about)
-    data = {
-            'name':circle.name,
-            'slug_name':circle.slug_name,
-            'rides_taken':circle.rides_taken,
-            'rides_offered':circle.rides_offered,
-            'members_limit':circle.members_limit
-    }
-
-    return Response(data)
+    serializer = CreateCircleSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    circle = serializer.save()
+    return Response(CircleSerializer(circle).data)
