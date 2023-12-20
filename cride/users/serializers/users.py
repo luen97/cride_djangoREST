b96da288d9ead36dcc92ccfc24a0fdc6cda8527e
Manuel Salazar <manalgom@gmail.com>
2023-12-19 19:08:04 -0500

    Add User verification to enable login

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -85,6 +85,8 @@ class UserLoginSerializer(serializers.Serializer):
 
         if not user:
             raise serializers.ValidationError('Invalid credentials')
+        if not user.is_verified:
+            raise serializers.ValidationError("Account is not active yet")
 
         self.context['user'] = user
         return data
