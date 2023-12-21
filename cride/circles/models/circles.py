d184726e3bd691d48462114b23d68d76d69cc463
Manuel Salazar <manalgom@gmail.com>
2023-12-21 15:21:43 -0500

    Add members attribute in the circle model and create the intermediate model Membership

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -21,6 +21,12 @@ class Circle(CRideModels):
     about = models.CharField('circle description', max_length=255)
     picture = models.ImageField(upload_to='circles/picture', blank=True, null=True)
 
+    members = models.ManyToManyField(
+        'users.User',
+        through='circles.Membership',
+        through_fields=('circle', 'user') # Fields names of Membership
+    )
+
     # Stats
     rides_offered = models.PositiveIntegerField(default=0)
     rides_taken = models.PositiveIntegerField(default=0)
