fd48b509da7ace8d3f1dda94d4636b0a6e69758f
Manuel Salazar <manalgom@gmail.com>
2023-12-22 11:22:48 -0500

    circle creation now makes the request user the admin, also validate members limit attr

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -9,6 +9,13 @@ from cride.circles.models import Circle
 class CircleModelSerializer(serializers.ModelSerializer):
     """Circle model serializer."""
 
+    members_limit = serializers.IntegerField(
+        required = False,
+        min_value=10,
+        max_value=1000
+    )
+    is_limited = serializers.BooleanField(default=False)
+
     class Meta:
         """Meta class"""
 
@@ -19,5 +26,19 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'rides_offered', 'rides_taken',
             'verified','is_public',
             'is_limited','members_limit'
-
         )
+
+        read_only_fields = (
+            'is_public',
+            'verified',
+            'rides_offered',
+            'rides_taken'
+        )
+
+    def validate(self,data):
+        """Ensure both members_limit and is_limited are present."""
+        members_limit = data.get('members_limit',None)
+        is_limited = data.get('is_limited',False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member list must be provided')
+        return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from cride.circles.serializers import CircleModelSerializer
 
 # Model
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 
 class CircleViewSet(viewsets.ModelViewSet):
@@ -25,3 +25,16 @@ class CircleViewSet(viewsets.ModelViewSet):
             return queryset.filter(is_public=True)
         return queryset
     
+    def perform_create(self,serializer):
+        """Assign a circle admin."""
+
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
