2d4d7390cfed3d1b6debde0637250fe418baabe5
Manuel Salazar <manalgom@gmail.com>
2024-01-22 21:03:49 -0500

    Invitation codes for the invitation retrieving

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -3,9 +3,11 @@
 # Django REST Framework
 from rest_framework import mixins,viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Models 
-from cride.circles.models import Circle, Membership
+from cride.circles.models import Circle, Membership, Invitation
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
@@ -58,6 +60,45 @@ class MembershipViewSet(mixins.ListModelMixin,
         """Disable membership"""
         instance.is_active=False
         instance.save()
-    
 
+    @action(detail=True,methods=['get'])
+    def invitations(self,request,*arggs,**kwargs):
+        """Retrieve a member's invitations breakdown
+        
+        Will return a list containing all the members that
+        has used its invitations and another list containing
+        the invitations that haven't being used yet.
+        """
+        member = self.get_object()
+
+        invited_members = Membership.objects.filter(
+            circle=self.circle,
+            invited_by=request.user,
+            is_active=True
+        )
+
+        unused_invitations = Invitation.objects.filter(
+            circle=self.circle,
+            issued_by=request.user,
+            used=False
+        ).values_list('code')
+
+        diff = member.remaining_invitations - len(unused_invitations)
+
+        invitations = [x[0] for x in unused_invitations]
+
+        for i in range(0,diff):
+            invitations.append(
+                Invitation.objects.create(
+                    issued_by=request.user,
+                    circle=self.circle
+                ).code
+            )
+
+        data = {
+            'used_invitations':MembershipModelSerializer(invited_members,many=True).data,
+            'invitations':invitations
+        }
+
+        return Response(data)
 
